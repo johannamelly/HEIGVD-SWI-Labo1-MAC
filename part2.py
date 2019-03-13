7ee103f4e66b638594f77610749d6d4c65ca156f
johannamelly <johannamelly@gmail.com>
2019-03-13 15:57:09 +0100

    part2 mais il manque le nom du réseau

--- part2.py
+++ part2.py
@@ -1,8 +1,12 @@
+import pprint
+import requests
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
+MAC_URL = 'http://macvendors.co/api/%s'
 stations = []
+
 
 def handler(packet):
 	# if packet has a 802.11 layer and is a data frame
@@ -13,14 +17,13 @@ def handler(packet):
 		# keeping a list of the stations
 		if (receiver not in stations):
 			stations.append(receiver)
-		# if we find the target, we print a confirmation
-		if(receiver == targetMAC):
-			print("Your client has been detected!")
-			packet.show()
 
-targetMAC = raw_input("Hello, please provide a MAC address: ")
 timer = raw_input("How long do you want me to search for this address (seconds)? ")
 timer = int(timer)
-
-print("Sniffing... please be patient, you'll be notified if your client is detected.")
+print("Sniffing... please be patient")
 sniff(iface="mon1", prn = handler, timeout=timer)
+
+for sta in stations:
+	r = requests.get(MAC_URL % sta)
+	res = r.json()['result']
+	print("%s (%s) - %s" % (sta, res['company'], res['address']))
