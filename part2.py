2fac2e52517eed8746a7e292a5c1792edc2533f2
johannamelly <johannamelly@gmail.com>
2019-03-20 16:52:29 +0100

    fini

--- part2.py
+++ part2.py
@@ -3,28 +3,28 @@ import requests
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+import json
 
 MAC_URL = 'http://macvendors.co/api/%s'
 stations = []
-
+keeptrace = []
 
 def handler(packet):
 	# if packet has a 802.11 layer and is a data frame
-	if(packet.haslayer(Dot11Beacon)):
-			print(packet.info)	
-			print(packet.addr3)
-			print("coucou")
 	if(packet.haslayer(Dot11)):
-#		if( packet.ID == 0):
-#			print("he :" + str(packet) )
-#			print("SSID ? " + packet.info)
 		if( packet.type == 2):
 			# getting station MAC receiving address
 			receiver = packet.getlayer(Dot11).addr1.upper()
-			
-			# keeping a list of the stations
-			if (receiver not in stations):
-				stations.append(receiver)
+			# keeping a list of the unique stations
+			if (receiver not in keeptrace):
+				if(packet.haslayer(Dot11Beacon)):
+					# get station and SSID
+					stations.append([receiver, packet.info])
+					keeptrace.append(receiver)
+				else:
+					# just get station
+					stations.append([receiver, '/'])
+					keeptrace.append(receiver)
 
 timer = raw_input("How long do you want me to search for this address (seconds)? ")
 timer = int(timer)
@@ -32,9 +32,14 @@ print("Sniffing... please be patient")
 sniff(iface="mon1", prn = handler, timeout=timer)
 
 for sta in stations:
-	r = requests.get(MAC_URL % sta)
-	res = r.json()['result']
-	if('error' in res):
-		print(sta)
-	else:
-		print("%s (%s) - %s" % (sta, res['company'], res['address']))
+	# requesting constructor
+	r = requests.get(MAC_URL % sta[0])
+	try:
+		res = r.json()['result']
+		# displaying
+		if('error' in res):
+			print("%s - %s" % (sta[0], sta[1]))
+		else:
+			print("%s (%s) - %s" % (sta[0], res['company'], sta[1]))
+	except ValueError, e:
+		print("%s - %s" % (sta[0], sta[1]))
--- rapport.md
+++ rapport.md
@@ -9,7 +9,9 @@ sudo airodump-ng mon1
 
 1. Quels types de trames sont nécessaires pour détecter les clients de manière passive?
 
+   Les probe requests permettent de le détecter.
+
 2. Pourquoi le suivi n'est-il plus possible sur iPhone depuis iOS 8?
 
-   Parce que iOS randomise les adresses MAC lors de la recherche WIFI
+   Parce que iOS randomise les adresses MAC lors de la recherche WIFI.
 
